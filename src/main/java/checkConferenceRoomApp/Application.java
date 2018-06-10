171c313b5e8540b044452658c008d169f54b0ed3
Piotr Skorupa <32395394+PiotrSkorupa@users.noreply.github.com>
2018-06-10 20:20:39 +0200

    cede refactor

--- src/main/java/checkConferenceRoomApp/Application.java
+++ src/main/java/checkConferenceRoomApp/Application.java
@@ -8,12 +8,13 @@ import checkConferenceRoomApp.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -52,4 +53,6 @@ public class Application {
             confRoomRepository.save(confRoomModel);
         };
     }
+
+
 }
